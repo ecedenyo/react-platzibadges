f12d6bcc52ecd96fd2d35dfa10cf4c16926db308
Emerson Cedeño <ecedenyo@gmail.com>
2021-06-29 03:56:19 -0500

    ✨ Adding list of badges in list view
    To do this we are incorporating to the project fontawesome for icons

---
+++ src/components/BadgesList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+export class BadgesList extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="list-unstyled">
+          {this.props.badges.map((badge) => {
+            return (
+              <li key={badge.id} className="shadow-sm p-3 mb-3 bg-body rounded">
+                <img
+                  src={badge.avatarUrl}
+                  alt="Avatar"
+                  className="float-start me-3"
+                />
+                <span>
+                  {badge.firstName} {badge.lastName}
+                </span>
+                <br />
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  className="text-primary me-1"
+                />
+                <a
+                  href={`https://twitter.com/${badge.twitter}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {badge.twitter}
+                </a>
+                <br />
+                <span>{badge.jobTitle}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BadgesList;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 
-import BadgeNew from './pages/BadgeNew';
+import Badges from './pages/Badges';
 
 const container = document.getElementById('app');
 
-render(<BadgeNew />, container);
+render(<Badges />, container);
