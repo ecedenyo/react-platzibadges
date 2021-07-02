f867b2bed5e0a5467d03c52a941b140dedacb465
Emerson Cedeño <ecedenyo@gmail.com>
2021-07-02 02:50:27 -0500

    ♻️ Encapsulating search function into a Custom Hook

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-function BadgesList(props) {
-  const badges = props.badges;
-
+function useSearchBadges(badges) {
   const [query, setQuery] = React.useState('');
   const [filteredBadges, setFilteredBadges] = React.useState(badges);
 
@@ -19,6 +17,14 @@ function BadgesList(props) {
 
     setFilteredBadges(result);
   }, [badges, query]);
+
+  return { query, setQuery, filteredBadges };
+}
+
+function BadgesList(props) {
+  const badges = props.badges;
+
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges);
 
   if (filteredBadges.length === 0) {
     return (
